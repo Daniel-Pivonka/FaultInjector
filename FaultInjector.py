110bff7008299a2c0fa47ea695e274e07155a819
Daniel Pivonka <dpivonka@dpivonka.bos.csb>
2017-06-16 12:19:12 -0400

    main

--- FaultInjector.py
+++ FaultInjector.py
@@ -16,17 +16,20 @@ def main():
 	parser.add_argument('-hw','--hardware', help='run hardware faults', required=False, action='store_true')
 	args = vars(parser.parse_args())
 
+
 	if args['process'] is True:
-	print 'process faults enabled'
-	f.write('{:%Y-%m-%d %H:%M:%S} process faults enabled\n'.format(datetime.datetime.now()))
+		print 'process faults enabled'
+		f.write('{:%Y-%m-%d %H:%M:%S} process faults enabled\n'.format(datetime.datetime.now()))
 
 	if args['system'] is True:
         print 'system faults enabled'
-	f.write('{:%Y-%m-%d %H:%M:%S} system faults enabled\n'.format(datetime.datetime.now()))
+		f.write('{:%Y-%m-%d %H:%M:%S} system faults enabled\n'.format(datetime.datetime.now()))
 
 	if args['hardware'] is True:
         print 'hardware faults enabled'
-	f.write('{:%Y-%m-%d %H:%M:%S} hardware faults enabled\n'.format(datetime.datetime.now()))
+		f.write('{:%Y-%m-%d %H:%M:%S} hardware faults enabled\n'.format(datetime.datetime.now()))
+
+
 
 	y = open('config.yaml', 'r')
 	f.write('{:%Y-%m-%d %H:%M:%S} Config file opened\n'.format(datetime.datetime.now()))
