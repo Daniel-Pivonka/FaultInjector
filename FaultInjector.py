99f25d5b42ba391f87dc516c7b0741b876aa094a
Daniel Pivonka <dpivonka@dpivonka.bos.csb>
2017-06-19 14:11:20 -0400

    prompt for flags if none entered

--- FaultInjector.py
+++ FaultInjector.py
@@ -8,7 +8,7 @@ import random
 import time
 
 
-debug = False
+debug = True
 
 # Node dictionary holds the node type as a key and
 # the node ip and if it's faultable as its value
@@ -41,44 +41,57 @@ def main():
             print 'process faults enabled'
         active_modes.append('process')
         log.write('{:%Y-%m-%d %H:%M:%S} process faults enabled\n'.format(datetime.datetime.now()))
-
     if args.system:
         if debug:
             print 'system faults enabled'
         active_modes.append('system')
         log.write('{:%Y-%m-%d %H:%M:%S} system faults enabled\n'.format(datetime.datetime.now()))
-
     if args.hardware:
         if debug:
             print 'hardware faults enabled'
         active_modes.append('hardware')
         log.write('{:%Y-%m-%d %H:%M:%S} hardware faults enabled\n'.format(datetime.datetime.now()))
 
-
-
-    #open config
+    #open config and parse
     with open('config.yaml', 'r') as f:
         config = yaml.load(f)
     log.write('{:%Y-%m-%d %H:%M:%S} Config file opened\n'.format(datetime.datetime.now()))
-
     parse_config(config)
-    service_fault('osd-compute', 'ceph', 5)
+
+    #test compatiblity
+    while not check_config_mode_compatiblity(active_modes):
+        #ask for new set of modes
+        new_flags = raw_input("You must enable at least one mode (-p, -s, -hw)\n")
+
+        if new_flags.find('-p') != -1:
+            if debug:
+                print 'process faults enabled'
+            active_modes.append('process')
+            log.write('{:%Y-%m-%d %H:%M:%S} process faults enabled\n'.format(datetime.datetime.now()))
+        if new_flags.find('-s') != -1:
+            if debug:
+                print 'system faults enabled'
+            active_modes.append('system')
+            log.write('{:%Y-%m-%d %H:%M:%S} system faults enabled\n'.format(datetime.datetime.now()))
+        if new_flags.find('-hw') != -1:
+            if debug:
+                print 'hardware faults enabled'
+            active_modes.append('hardware')
+            log.write('{:%Y-%m-%d %H:%M:%S} hardware faults enabled\n'.format(datetime.datetime.now()))
+
+    #compatibiliy checks out
 
     #runtime info
     if debug:
         print 'fault injector will run for {} minutes' .format(args.timelimit)
     log.write('{:%Y-%m-%d %H:%M:%S} fault injector will run for {} minutes\n'.format(datetime.datetime.now(), args.timelimit))    
 
+    run_injector(args.timelimit, active_modes, log)
 
-    #test compatiblity
-    if check_config_mode_compatiblity(active_modes):
+    #timelimit reached ask if user wants more time 
 
-        run_injector(args.timelimit, active_modes, log)
-        #timelimit reached ask if user wants more time 
 
-    else:
-        print "you must enable atleast one mode"
-        #ask for new set of modes
+        
 
 
     
