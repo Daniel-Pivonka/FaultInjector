2e6ea47f594aa95ca953f4991c1515d264cfe842
Steven Brzozowski <sbrzozow@sbrzozow.bos.csb>
2017-06-19 10:11:54 -0400

    Added parse_config() and a corresponding node dictionary to store each node, its ip, and its status.

--- FaultInjector.py
+++ FaultInjector.py
@@ -6,6 +6,17 @@ import subprocess
 import argparse
 
 
+debug = True
+
+# Node dictionary holds the node type as a key and
+# the node ip and if it's faultable as its value
+nodes = {
+    
+    'controller': [],
+    'osd-compute': []
+
+}
+
 def main():
     #open log file
     log = open('FaultInjector.log', 'a')
@@ -23,15 +34,15 @@ def main():
 
 
     #check mode args
-    if args['process'] is True:
+    if args['process']:
         print 'process faults enabled'
         log.write('{:%Y-%m-%d %H:%M:%S} process faults enabled\n'.format(datetime.datetime.now()))
 
-    if args['system'] is True:
+    if args['system']:
         print 'system faults enabled'
         log.write('{:%Y-%m-%d %H:%M:%S} system faults enabled\n'.format(datetime.datetime.now()))
 
-    if args['hardware'] is True:
+    if args['hardware']:
         print 'hardware faults enabled'
         log.write('{:%Y-%m-%d %H:%M:%S} hardware faults enabled\n'.format(datetime.datetime.now()))
 
@@ -42,12 +53,14 @@ def main():
         config = yaml.load(f)
     log.write('{:%Y-%m-%d %H:%M:%S} Config file opened\n'.format(datetime.datetime.now()))
 
-
+    parse_config(config)
 
     #test compatiblity
-    if check_config_mode_compatiblity() is True:
+    if check_config_mode_compatiblity():
         #pick mode
+        pass
     else:
+        pass
         #ask for new set of modes
 
 
@@ -68,14 +81,36 @@ def main():
     #end
 
 
-def check_config_mode_compatiblity()
+def check_config_mode_compatiblity():
+    pass
 
-def service_fault()
+def service_fault(downtime):
+    """ Kills the ceph service of a random node for downtime seconds.
+        Does not check if the node can be faulted.
+    """
+    pass
 
-def node_fault()
+def node_fault():
+    pass
 
-def hardware_fault()
+def hardware_fault():
+    pass
 
+def parse_config(config):
+    """ Iterates through the config.yaml file, placing
+        each node and corresponding ip in the appropiate
+        place in the node dictionary.
+    """
+    for node_index in range(config['numnodes']):
+        current_node = config['node' + str(node_index)]
+        node_type = current_node['type']
+        nodes[node_type].append(((current_node['ip']), True))
+
+    if debug:
+        print "Nodes from config:"
+        for node in nodes:
+            print node, nodes[node]
 
     
-if __name__ == "__main__":main()
+if __name__ == "__main__":
+    main()
