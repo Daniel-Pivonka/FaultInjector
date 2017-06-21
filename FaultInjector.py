3b78bd9ba2e1c66da43026ddbe2b2e8c06bf3f15
Daniel Pivonka <dpivonka@dpivonka.bos.csb>
2017-06-21 15:39:23 -0400

    remove debug

--- FaultInjector.py
+++ FaultInjector.py
@@ -38,13 +38,7 @@ def main():
     parser.add_argument('-s','--system', help='run system faults', required=False, action='store_true')
     parser.add_argument('-hw','--hardware', help='run hardware faults', required=False, action='store_true')
     parser.add_argument('-t','--timelimit', help='timelimit for injector to run (mins) default 30 mins', required=False, type=int, default=30, metavar='\b')
-    parser.add_argument('-d','--debug', help='print outs injector info', required=False, action='store_true')
     args = parser.parse_args()
-
-    #debug info
-    if args.debug:
-        global debug
-        debug = True
 
 
     #list to hold active modes to be randomly chosen
