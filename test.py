f521dbaf092fe1c58779ad7088b9c7e94ff06a50
Daniel Pivonka <dpivonka@dpivonka.bos.csb>
2017-06-28 14:09:49 -0400

    structure

--- test.py
+++ test.py
@@ -1,69 +1,108 @@
 #!/usr/bin/python
 
-import threading
-import time
+import argparse
+import datetime
+# import threading
+# import time
 
 class Fault:
-	def stateless(self):
-		raise NotImplementedError
+    def stateless(self):
+        raise NotImplementedError
 
-	def stateful(self):
-		raise NotImplementedError
+    def stateful(self):
+        raise NotImplementedError
 
-	def deterministic(self):
-		raise NotImplementedError
+    def deterministic(self):
+        raise NotImplementedError
 
 
 class Ceph(Fault):
+    def stateless(self):
+        print "ceph stateless"
+
+    def stateful(self):
+        print "ceph stateful"
+
+    def deterministic(self):
+        print "ceph deterministic"
+
+
+
+# global var for log file
+log = open('FaultInjector.log', 'a')
 
 def main():
 
+    # start injector
+    log.write('{:%Y-%m-%d %H:%M:%S} Fault Injector Started\n'.format(datetime.datetime.now()))
+
+    # create argument parser
+    parser = argparse.ArgumentParser(description='Fault Injector')
+    parser.add_argument('-d','--deterministic', help='injector will follow the list of tasks in the file specified', action='store', nargs=1, dest='filepath')
+    parser.add_argument('-sf','--stateful', help='injector will run in stateful random mode', required=False, action='store_true')
+    parser.add_argument('-sl','--stateless', help='injector will run in statelss random mode', required=False, action='store_true')
+    args = parser.parse_args()
+
+    # check mode
+    if args.filepath:
+        deterministic()
+    elif args.stateful:
+        stateful()
+    elif args.stateless:
+        stateless()
+    else:
+        print "No Mode Chosen"
+
+    # end injector
+    log.write('{:%Y-%m-%d %H:%M:%S} Fault Injector Stopped\n'.format(datetime.datetime.now()))
+    log.close()
+
+
+def deterministic():
+    print "det"
+
+def stateful():
+    print "sf"
+
+def stateless():
+    print "sl"
 
 
 
 
 
 
+#   print "main"
+#   t1 = threading.Thread(target=thread1)
+#   t2 = threading.Thread(target=thread2)
+#   t3 = threading.Thread(target=thread3)
+#   t1.start()
+#   t2.start()
+#   t3.start()
 
+#   t1.join()
+#   t2.join()
+#   t3.join()
 
-
-
-
-
-
-
-
-# 	print "main"
-# 	t1 = threading.Thread(target=thread1)
-# 	t2 = threading.Thread(target=thread2)
-# 	t3 = threading.Thread(target=thread3)
-# 	t1.start()
-# 	t2.start()
-# 	t3.start()
-
-# 	t1.join()
-# 	t2.join()
-# 	t3.join()
-
-# 	print "done"
+#   print "done"
 
 # def thread1():
-# 	time.sleep(5)
-# 	print "im the thread1"
-# 	time.sleep(5)
-# 	print "thread1 again"
+#   time.sleep(5)
+#   print "im the thread1"
+#   time.sleep(5)
+#   print "thread1 again"
 
 # def thread2():
-# 	time.sleep(5)
-# 	print "im the thread2"
-# 	time.sleep(5)
-# 	print "thread2 again"
+#   time.sleep(5)
+#   print "im the thread2"
+#   time.sleep(5)
+#   print "thread2 again"
 
 # def thread3():
-# 	time.sleep(5)
-# 	print "im the thread3"
-# 	time.sleep(5)
-# 	print "thread3 again"
+#   time.sleep(5)
+#   print "im the thread3"
+#   time.sleep(5)
+#   print "thread3 again"
 
 
 if __name__ == "__main__":
