a72d99ed7f33513983c4873ec098a5162873f8d9
Steven Brzozowski <sbrzozow@sbrzozow.bos.csb>
2017-06-30 15:04:37 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import paramiko
+import yaml 
+
+"""
+The purpose of this file is to attempt to fill the config file
+as thouroughly as possible. Note that some parameters in the config
+like "HCI" may not be filled in certain cases, so the config should
+be checked before running the main program
+"""
+
+
+
+
+# Open config file  
+f = open('playbooks/ceph-osd-fault-restore.yml', 'w')
+config = yaml.load(f)
+
+controller_response = subprocess.check_output(['. stackrc && nova list | grep control'])
+
+print controller_response 
+
+replica_size_command = 'ceph osd pool ls detail -f json'
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(, username='heat-admin')
+ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(replica_size_command)
+replica_response = str(ssh_stdout.readlines())
+print replica_response
+
+
+# Dump changes to file and close it
+yaml.dump(config, f, default_flow_style=False)
+f.close()
