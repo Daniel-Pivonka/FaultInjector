72ad25e8f905a553b4e47ecbaec688303e5405e7
Steven Brzozowski <sbrzozow@sbrzozow.bos.csb>
2017-07-03 15:36:44 -0400

    updated

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ for line in node_response:
 	node_fields = line[1:-1].split('|')
 	node_id = node_fields[0].strip()
 	node_type = node_fields[1].partition('-')[-1].rpartition('-')[0]
-	node_name = node_fields[1].partition('-')[-1].rpartition(' ')[0]
+	node_name = node_fields[1].partition('-')[-1].rpartition(' ')[0].strip()
 	if node_type == 'osd-compute':
 		config['deployment']['hci'] = True
 	node_ip = node_fields[5].partition('=')[-1].strip()
