1b67c84bb2ffdd8b14843c03c535ce33a160ffc8
Steven Brzozowski <sbrzozow@sbrzozow.bos.csb>
2017-07-03 08:20:32 -0400

    updated

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect('192.168.24.13', username='heat-admin')
 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(replica_size_command)
-replica_response = ssh_stdout.readlines()
+replica_response = ssh_stdout.read()
 ssh_stdout.channel.close()
 print replica_response
 
