5545fe7881d171f61ef56bec6f0b218d23f4b67c
Steven Brzozowski <sbrzozow@sbrzozow.bos.csb>
2017-07-03 12:10:26 -0400

    updated

--- test.py
+++ test.py
@@ -297,7 +297,7 @@ class Deployment:
         with open(filename, 'r') as f:
             config = yaml.load(f)
             for node_id in config['deployment']['nodes']:
-                self.nodes.append(Node(config['deployment']['nodes'][node_id]['node_type'], config['deployment']['nodes'][node_id]['node_ip'], node_ip))
+                self.nodes.append(Node(config['deployment']['nodes'][node_id]['node_type'], config['deployment']['nodes'][node_id]['node_ip'], node_id))
                 self.hci = config['deployment']['hci']
                 self.containerized = config['deployment']['containerized']
                 self.num_nodes = config['deployment']['num_nodes']
