e8c48b5125a3cc74bb98807ae2b92344d180e4b1
Steven Brzozowski <sbrzozow@sbrzozow.bos.csb>
2017-07-03 10:55:40 -0400

    updated

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ for line in node_response:
 	print node_fields
 	node_id = node_fields[0].strip()
 	print node_id 
+	print 'node fields:\n', node_fields
 	node_type = node_fields[1].partition('-')#[-1]#.rpartition('-')[0]
 	print node_type
 	#print "\n", line
