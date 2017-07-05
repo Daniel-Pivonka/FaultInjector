e40c1ba3e462de4e37febbfb509bb189ae1a489a
Steven Brzozowski <sbrzozow@sbrzozow.bos.csb>
2017-07-05 08:47:34 -0400

    updated

--- setup.py
+++ setup.py
@@ -95,6 +95,8 @@ if args.activate_ceph:
 				print 'name to match:', config['deployment']['nodes'][node_id]['node_name']
 				if node_name == config['deployment']['nodes'][node_id]['node_name']:
 					print "HERE"
+					print config['deployment']['nodes'][node_id]['num_osds']
+					print len(json_response[ceph_node]['children'])
 					config['deployment']['nodes'][node_id]['num_osds'] = len(json_response[ceph_node]['children'])
 
 
