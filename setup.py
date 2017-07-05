4ca6457dee36b7313eba78b97bf51a7e0c69fd66
Steven Brzozowski <sbrzozow@sbrzozow.bos.csb>
2017-07-05 08:43:36 -0400

    updated

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ if args.activate_ceph:
 
 	# Count number of osds in each node and assign them appropriately 	
 	for ceph_node in json_response['nodes']:
-		node_name = ceph_node['name'].rpartition('-')[-1]
+		node_name = ceph_node['name'].partition('-')[-1]
 		for node_id in config['deployment']['nodes']:
 			if (config['deployment']['nodes'][node_id]['node_type'] == 'osd-compute') \
 				or (config['deployment']['nodes'][node_id]['node_type'] == 'ceph'):
