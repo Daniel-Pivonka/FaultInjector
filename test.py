3fd8efa3d180ddebd3dfd121a3a5fbab0e1bfbc1
Steven Brzozowski <sbrzozow@sbrzozow.bos.csb>
2017-07-07 15:36:15 -0400

    updated

--- test.py
+++ test.py
@@ -208,6 +208,12 @@ class Ceph(Fault):
             result = random.choice(self.functions)()
             if result is None:
                 continue
+            # Add space if ip is short
+            if len(result[2]) == 12:
+                result[2] = result[2] + '  '
+            elif len(result[2]) == 13
+                result[2] = result[2] + ' '
+
             deterministic_file.write(self.__repr__() + ' | ' + str(result[0]) + 
                                     ' | ' + str(result[1]) + ' | ' + str(result[2]) + 
                                      ' | ' + str(result[3]) + ' | ' + str(result[4]) + 
