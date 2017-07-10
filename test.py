6bb013818e03db1f105dab30b7e617ccba7b3fa9
Daniel Pivonka <dpivonka@dpivonka.bos.csb>
2017-07-10 09:44:41 -0400

    test

--- test.py
+++ test.py
@@ -294,6 +294,9 @@ class Ceph(Fault):
     # Write fault functions below --------------------------------------------- 
 
     def fault_thread(self, deterministic_file, timelimit):
+
+        print "thread started"
+
         # Infinite loop for indefinite mode
         while timelimit is None:
             result = random.choice(self.functions)()
