1d65c3019fa4c3f72034e8a1b9ba52ac3e488a99
stevenbrz <stevenbrz8@gmail.com>
2017-07-11 12:44:17 -0400

    Updated

--- FaultInjector.py
+++ FaultInjector.py
@@ -760,8 +760,8 @@ class Ceph(Fault):
                 if node[2]:
                     self.deployment.mons_available += 1
 
-        print "---------------" \
-              "Current Status:\n" \
+        print "Current Status:\n" \
+              "---------------\n" \
               "osds active: " + str(self.deployment.num_osds - osds_occupied) + '/' + str(self.deployment.num_osds) + '\n' \
               "monitors active: " + str(self.deployment.mons_available) + '/' + str(self.deployment.num_mons) +'\n' \
               "---------------"
