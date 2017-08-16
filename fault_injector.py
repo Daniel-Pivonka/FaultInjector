c7963df10f4e8d2abe9fbeda57d5d52261943c22
stevenbrz <stevenbrz8@gmail.com>
2017-08-16 15:52:49 -0400

    reformatted

--- fault_injector.py
+++ fault_injector.py
@@ -4,6 +4,7 @@ import argparse
 import datetime
 import math
 import os
+import paramiko
 import random
 import re
 import signal
@@ -13,7 +14,7 @@ import sys
 import threading
 import time
 import yaml
-import paramiko
+
 
 class Fault:
     """ Template class to make custom fault class
